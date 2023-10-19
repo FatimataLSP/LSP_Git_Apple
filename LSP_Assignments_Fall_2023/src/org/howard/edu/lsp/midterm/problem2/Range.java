8037f2dc9955acca168a006137e8e18b8e067a28
tima <tima@fatimas-mbp>
2023-10-19 19:20:30 -0400

    First commit of midterm problems 1&2

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/problem2/Range.java
@@ -0,0 +1,23 @@
+package org.howard.edu.lsp.midterm.problem2;
+
+public class Range {
+	private int start;
+    private int end;
+
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public boolean isInRange(int value) {
+        return value >= start && value <= end;
+    }
+
+    public boolean overlaps(Range otherRange) {
+        return (start <= otherRange.end && end >= otherRange.start);
+    }
+
+    public int size() {
+        return Math.abs(end - start) + 1;
+    }
+}
